56b44afcc36cbf6178f75d8256f8a9d655f27826
Aleksey Belchenkov <u608110@gmail.com>
2021-07-02 19:37:06 +0400

    Login & generate JWT token

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { TypegooseModule } from "nestjs-typegoose";
+import { ConfigModule, ConfigService } from "@nestjs/config";
 
 import { UserModel } from "./user.model";
 import { AuthService } from './auth.service';
+import { JwtModule } from "@nestjs/jwt";
+import { getJWTConfig } from "../configs/jwt.config";
 
 @Module({
   controllers: [AuthController],
@@ -15,7 +18,12 @@ import { AuthService } from './auth.service';
           collection: 'User'
         }
       }
-    ])
+    ]),
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: getJWTConfig
+    })
   ],
   providers: [AuthService]
 })

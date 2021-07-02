262ae987ad4462c69714da6c96f1b6584837bf88
Aleksey Belchenkov <u608110@gmail.com>
2021-07-02 19:53:43 +0400

    JWT Strategy & Guard

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -2,11 +2,13 @@ import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { TypegooseModule } from "nestjs-typegoose";
 import { ConfigModule, ConfigService } from "@nestjs/config";
+import { PassportModule } from "@nestjs/passport";
+import { JwtModule } from "@nestjs/jwt";
 
 import { UserModel } from "./user.model";
 import { AuthService } from './auth.service';
-import { JwtModule } from "@nestjs/jwt";
 import { getJWTConfig } from "../configs/jwt.config";
+import { JwtStrategy } from "./strategies/jwt.strategy";
 
 @Module({
   controllers: [AuthController],
@@ -19,12 +21,17 @@ import { getJWTConfig } from "../configs/jwt.config";
         }
       }
     ]),
+    ConfigModule,
     JwtModule.registerAsync({
       imports: [ConfigModule],
       inject: [ConfigService],
       useFactory: getJWTConfig
-    })
+    }),
+    PassportModule
   ],
-  providers: [AuthService]
+  providers: [
+    AuthService,
+    JwtStrategy
+  ]
 })
 export class AuthModule {}
--- src/review/review.controller.ts
+++ src/review/review.controller.ts
@@ -7,12 +7,14 @@ import {
     HttpStatus,
     Param,
     Post,
+    UseGuards,
     UsePipes,
     ValidationPipe
 } from "@nestjs/common";
 
 import { CreateReviewDto } from "./dto/create-review.dto";
 import { ReviewService } from "./review.service";
+import { JwtGuard } from "../auth/guards/jwt.guard";
 import { REVIEW_NOT_FOUND } from "./review.constants";
 
 @Controller('review')
@@ -28,6 +30,7 @@ export class ReviewController {
         return  this.reviewService.create(dto);
     }
 
+    @UseGuards(JwtGuard)
     @Delete(':id')
     async delete(@Param('id') id: string) {
         const deletedDoc = await this.reviewService.delete(id);
